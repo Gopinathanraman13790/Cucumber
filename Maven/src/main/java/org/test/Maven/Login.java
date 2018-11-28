1195300237180446615682937c798398514574cd
unknown <Gopinathanraman13790>
2018-11-28 13:46:13 +0530

    single and multiple values using list and maps

--- Maven/src/main/java/org/test/Maven/Login.java
+++ Maven/src/main/java/org/test/Maven/Login.java
@@ -1,10 +1,16 @@
 package org.test.Maven;
 
+
+
+import java.util.List;
+import java.util.Map;
+
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import cucumber.api.DataTable;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -22,14 +28,63 @@ public class Login {
 	    
 	}
 
-	@When("^The user enter the valid username,password and click login button$")
-	public void the_user_enter_the_valid_username_password_and_click_login_button() {
+//	@When("^The user enter the valid username,password and click login button$")
+//	public void the_user_enter_the_valid_username_password_and_click_login_button() {
 	    // Write code here that turns the phrase above into concrete actions
-	    driver.findElement(By.xpath("//input[@name='uid']")).sendKeys("mngr165242");
-	    driver.findElement(By.xpath("//input[@name='password']")).sendKeys("nYjadAs");
-	    driver.findElement(By.xpath("//input[@name='btnLogin']")).click();
+//	    driver.findElement(By.xpath("//input[@name='uid']")).sendKeys("mngr165242");
+//	    driver.findElement(By.xpath("//input[@name='password']")).sendKeys("nYjadAs");
+	   
+//		
+//		}
+	
+	@When("^The user enter the valid username,password and click login button$")
+	public void the_user_enter_the_valid_username_password_and_click_login_button(DataTable credentials) {
+	    // Write code here that turns the phrase above into concrete actions
+	    // For automatic transformation, change DataTable to one of
+	    // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
+	    // E,K,V must be a scalar (String, Integer, Date, enum etc).
+	    // Field names for YourType must match the column names in 
+	    // your feature file (except for spaces and capitalization).
+	    // using List 
+//		List<String> credentialList = credentials.asList(String.class);	
+//		System.out.println(credentialList);
+//	    driver.findElement(By.xpath("//input[@name='uid']")).sendKeys(credentialList.get(0));
+//        driver.findElement(By.xpath("//input[@name='password']")).sendKeys(credentialList.get(1) );
 		
-		}
+		// map based using key and value
+//		Map<String, String> cresentialMap = credentials.asMap(String.class, String.class);
+//		System.out.println(cresentialMap);
+//		driver.findElement(By.xpath("//input[@name='uid']")).sendKeys(cresentialMap.get("UserName"));
+//       driver.findElement(By.xpath("//input[@name='password']")).sendKeys(cresentialMap.get("Password"));
+//	    
+		// multiple values using list
+//		List<List<String>> credentialLists = credentials.asLists(String.class);
+//		System.out.println(credentialLists);
+//		System.out.println(credentialLists.get(2).get(0));
+//		System.out.println(credentialLists.get(2).get(1));
+//		System.out.println(credentialLists.get(3).get(0));
+//		System.out.println(credentialLists.get(3).get(1));
+//		driver.findElement(By.xpath("//input[@name='uid']")).sendKeys(credentialLists.get(0).get(0));
+//       driver.findElement(By.xpath("//input[@name='password']")).sendKeys(credentialLists.get(0).get(1));
+//		
+		// multiple values using list
+		List<Map<String, String>> cresentialMaps = credentials.asMaps(String.class, String.class );
+		System.out.println(cresentialMaps);
+		System.out.println(cresentialMaps.get(1).get("Password"));
+		System.out.println(cresentialMaps.get(3).get("UserName"));
+		driver.findElement(By.xpath("//input[@name='uid']")).sendKeys(cresentialMaps.get(0).get("UserName"));
+      driver.findElement(By.xpath("//input[@name='password']")).sendKeys(cresentialMaps.get(0).get("Password"));
+		
+		
+	}
+
+	
+	@When("^The user clicks the login button$")
+	public void the_user_clicks_the_login_button() {
+	    // Write code here that turns the phrase above into concrete actions
+		 driver.findElement(By.xpath("//input[@name='btnLogin']")).click();
+	}
+
 
 	@Then("^the user should be in managers home page$")
 	public void the_user_should_be_in_managers_home_page() {
--- Maven/target/classes/org/test/Maven/Login.feature
+++ Maven/target/classes/org/test/Maven/Login.feature
@@ -1,10 +1,40 @@
 #Author: your.email@your.domain.com
-
-
 Feature: To test Login functionality
- 
-  Scenario: verify login with valid crediential
+
+  #using list
+  #  Scenario: verify login with valid crediential
+  #    Given The user in the demo guru page
+  #    When The user enter the valid username,password and click login button
+  #      | mngr165242 | nYjadAs |
+  #    And The user clicks the login button
+  #    Then the user should be in managers home page
+  #using Map
+  # Scenario: verify login with valid crediential
+  #   Given The user in the demo guru page
+  #   When The user enter the valid username,password and click login button
+  #     | UserName | mngr165242 |
+  #     | Password | nYjadAs    |
+  #   And The user clicks the login button
+  #   Then the user should be in managers home page
+  #using multiple values using List
+ # Scenario: verify login with valid crediential
+ #   Given The user in the demo guru page
+ #   When The user enter the valid username,password and click login button
+ #     | mngr165242 | nYjadAs   |
+ #     | user2      | password2 |
+ #     | user3      | password3 |
+ #     | user4      | password4 |
+ #   And The user clicks the login button
+ #   Then the user should be in managers home page
+ #using multiple values using map
+     Scenario: verify login with valid crediential
     Given The user in the demo guru page
-    When  The user enter the valid username,password and click login button
+    When The user enter the valid username,password and click login button
+      |UserName    | Password  |
+      | mngr165242 | nYjadAs   |
+      | user2      | password2 |
+      | user3      | password3 |
+      | user4      | password4 |
+    And The user clicks the login button
     Then the user should be in managers home page
-     
+    
